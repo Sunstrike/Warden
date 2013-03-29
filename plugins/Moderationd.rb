6aa6898e09e96d35fb33630baf3c7ab6e820738e
Sunstrike <sunstrike@azurenode.net>
2013-03-29 13:21:44 +0000

    Rubymine stuff, update headers.
    Signed-off-by: Sunstrike <sunstrike@azurenode.net>

--- plugins/Moderationd.rb
+++ plugins/Moderationd.rb
@@ -5,7 +5,7 @@
 #
 # AZI IRC moderation bot
 # 
-# Copyright (C) 2012 Robert Tully
+# Copyright (C) 2012 Sunstrike <sunstrike@azurenode.net>
 # 
 # Permission is hereby granted, free of charge, to any person
 # obtaining a copy of this software and associated documentation
